c36798789cbd948f33017d022c56e58f8a167eab
Heron <heronmoy1234@gmail.com>
2024-07-10 21:03:04 +0600

    Fvm add

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -2,7 +2,7 @@ class ApiUrl {
   static const String _baseUrl = "https://api.openweathermap.org/data/3.0/onecall";
 
   //ADD_YOUR_OPENWEATHERMAP_API_KEY
-  static const String _apiKey = "203b08bce6eba986aa77c5ff88783650";
+  static const String _apiKey = "ADD_YOUR_OPENWEATHERMAP_API_KEY";
 
   static String weatherApi(lat, lon) =>
       "$_baseUrl?lat=$lat&lon=$lon&appid=$_apiKey&units=metric&exclude=minutely";
