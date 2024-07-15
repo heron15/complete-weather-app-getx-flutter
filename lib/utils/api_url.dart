9f2960fc43a21d94d49244d56963c2c6000a2ff6
Heron <heronmoy1234@gmail.com>
2024-07-15 14:01:57 +0600

    graph data complete

--- lib/utils/api_url.dart
+++ lib/utils/api_url.dart
@@ -2,13 +2,13 @@ class ApiUrl {
   //weather api part
   static const String _weatherBaseUrl = "https://api.openweathermap.org/data/3.0/onecall";
   static const String _apiKey =
-      "ADD_YOUR_OPEN_WEATHER_MAP_API_KEY"; //ADD_YOUR_OPEN_WEATHER_MAP_API_KEY
+      "203b08bce6eba986aa77c5ff88783650"; //ADD_YOUR_OPEN_WEATHER_MAP_API_KEY
   static String weatherApi(lat, lon) =>
       "$_weatherBaseUrl?lat=$lat&lon=$lon&appid=$_apiKey&units=metric&exclude=minutely";
 
   //google cloud api part
   static const String _placeApiKey =
-      "ADD_YOUR_GOOGLE_PLACE_API_KEY"; //ADD_YOUR_GOOGLE_PLACE_API_KEY
+      "AIzaSyCgFBWG93liXqwHQyB5y7HcrS8UHfagu9c"; //ADD_YOUR_GOOGLE_PLACE_API_KEY
 
   static const String _googlePlaceAPiBaseURL =
       'https://maps.googleapis.com/maps/api/place/autocomplete/json';
